21402ae19c40334cd010e4d57d74192c9eca3c77
vegodiego <53877309+vegodiego@users.noreply.github.com>
2020-04-11 22:13:49 -0500

    app ok

--- src/App.js
+++ src/App.js
@@ -2,13 +2,27 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+  	super();
+
+  	this.state={
+  		number:0
+  	};
+  	this.increment=this.increment.bind(this);
+  }
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.number}</span>
+        <button id="inc" onClick={this.increment}>Incrementa</button>
       </div>
     );
+  }
+
+  increment(){
+  	this.setState({
+  		number:this.state.number+=1
+  	});
   }
 }
 
